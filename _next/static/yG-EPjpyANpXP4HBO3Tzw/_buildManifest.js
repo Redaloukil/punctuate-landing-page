6c599b8ebabbe877f652b0f78b2102557a4b6e1e
Reda Loukil <reda.loukil@klm.com>
2024-08-04 23:09:34 +0200

    initial commit : make the backbone of the landing page

---
+++ _next/static/yG-EPjpyANpXP4HBO3Tzw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/221-73eec6a17deed9e1.js","static/chunks/pages/index-3560d131ac3a40f5.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
